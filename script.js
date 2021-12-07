736eada0b4664c2adeee1902db7e8b241b65a050
freeburma <kophyoblue@gmail.com>
2021-12-07 14:20:15 +1300

    Better rendering with window.requestAnimationFrame

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 let num = 1; 
-let xPos = 0; 
-let yPos = 0;
 
 let max_xPos = window.screen.width - 200;
 let max_yPos = window.screen.height - 200;
@@ -16,10 +14,21 @@ function CreateBubbleContainer()
                             `; 
 }
 
+function GenerateRandomPosition(id)
+{
+    console.log(`Id: ${id}`);
+
+    let l_xPos = Math.floor(Math.random() * max_xPos); 
+    let l_yPos = Math.floor(Math.random() * max_yPos); 
+
+
+    return { l_xPos, l_yPos }; 
+}
+
 function GenerateRandomPosition()
 {
-    let l_xPos = Math.random() * max_xPos; 
-    let l_yPos = Math.random() * max_yPos; 
+    let l_xPos = Math.floor(Math.random() * max_xPos); 
+    let l_yPos = Math.floor(Math.random() * max_yPos); 
 
 
     return { l_xPos, l_yPos }; 
@@ -31,44 +40,74 @@ function GenerateRandomPosition()
 
 console.log(`Screen: ${max_xPos} : ${max_yPos} px`);
 
+let isMove = true; 
+
+function init()
+{
+    window.requestAnimationFrame(InterValFunction)
+}
+
 function InterValFunction()
 {
-    console.log(`${num} : margin ${xPos}`);
     let boxElement = document.getElementById('bubble_1'); 
 
-    //// Generating the Random Num
-    let generateCoordinate = GenerateRandomPosition(); 
 
-    boxElement.style.left = generateCoordinate.l_xPos + 'px'; 
-    boxElement.style.top = generateCoordinate.l_yPos + 'px'; 
-
-    // let windowWidth = window.screen.width; 
-    // let windowHeight = window.screen.height; 
-
-    //// Checking the screen width
-    // if ( xPos < max_xPos && yPos < max_xPos)
-    // {
-    //     xPos += 100; 
-    //     yPos += 100; 
-    // }
-    // else 
-    // {
-    //     xPos = 100; 
-    //     yPos = 100; 
-    // }
-
-    if ( xPos < max_xPos && yPos < max_xPos)
+    if (isMove)
     {
-        xPos = 0; 
-        yPos = 0; 
+
+        //// Generating the Random Num
+        let generateCoordinate = GenerateRandomPosition(); 
+        // console.log(`[x: ${generateCoordinate.l_xPos}, y: ${generateCoordinate.l_yPos}]`);
+
+
+        boxElement.style.left = generateCoordinate.l_xPos + 'px'; 
+        boxElement.style.top = 100 + 'px'; 
+
+        isMove = false; 
+    }// end if 
+
+    let rect = boxElement.getBoundingClientRect(); 
+    // console.log(rect);
+
+    let xPos = rect["x"]; 
+
+    if (xPos >= max_xPos)
+    {
+        xPos = 100; 
     }
 
 
-    num ++; 
+    let yPos = rect["y"]; 
+    if (yPos >= max_yPos)
+    {
+        yPos = 100; 
+    } 
 
+
+    
+    xPos += 1; 
+    // yPos += 20; 
+
+    boxElement.style.left = xPos + 'px'; 
+    boxElement.style.top = yPos + 'px'; 
+
+    // console.log(`[xR: ${rect["x"]}, yR: ${rect["y"]}] => [x: ${xPos}, y: ${yPos}]`);
+
+    window.requestAnimationFrame(InterValFunction); 
+
+    
 }
 
-setInterval(() => InterValFunction(), 1000); 
+init(); 
+
+// setInterval(() => InterValFunction(), 1000); 
+
+// while(true)
+// {
+    
+
+//     setTimeout(InterValFunction, 10); 
+// }// end while
 
 
 // CreateBubbleContainer(); 
